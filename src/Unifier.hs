493645ab0abb311aca25509d159f8eb5256c23d3
NoHomey <ivo.stratev.tues@gmail.com>
2018-12-30 15:34:51 +0200

    removing extra parentheses

--- src/Unifier.hs
+++ src/Unifier.hs
@@ -24,11 +24,11 @@ step g@(e@(a, b):es) = if a == b
                                   (Const _, Const _) -> Just (Check, g)
                                   (Const _, Var x) -> Just (Eliminate (x, a), es)
                                   (Func _ _, Var _) -> step $ (b, a):es
-                                  (Var x, Func f p) -> Just $ if (x `elem` (vars b))
+                                  (Var x, Func f p) -> Just $ if x `elem` (vars b)
                                                                 then (Check, g)
                                                                 else (Eliminate (x, b), es)
                                   (Var x, t) -> Just (Eliminate (x, t), es)
-                                  (Func f p1, Func h p2) -> if ((f /= h) || (((/=) `on` fArity) a b))
+                                  (Func f p1, Func h p2) -> if (f /= h) || (((/=) `on` fArity) a b)
                                                               then Just (Conflict, g)
                                                               else nextStep ((zip p1 p2) ++ es) Decompose
                                   e -> do
@@ -59,7 +59,7 @@ tryUnify g = let s = step g
                            (Check, _) -> Nothing
                            (Eliminate (x, t), g') -> do
                                                          us <- tryUnify $ map (\(y, r) -> (y, substitute x t r)) g'
-                                                         return $ (x, (foldr (\(y, r) t' -> substitute y r t') t us)):us
+                                                         return $ (x, foldr (\(y, r) t' -> substitute y r t') t us):us
                     
 unify :: Atom -> Atom -> Maybe Unifier
 unify a b = do
