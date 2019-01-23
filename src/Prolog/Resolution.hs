7d4cb8b9b289f90aee2b86ee2fe2a3f9840174c8
NoHomey <ivo.stratev.tues@gmail.com>
2019-01-23 11:38:10 +0200

    refactoring

--- src/Prolog/Resolution.hs
+++ src/Prolog/Resolution.hs
@@ -27,11 +27,9 @@ type Substitution s v = U.Substitution s (v, v)
 
 type Rules p s v = OU.Rules p s v
 
-type Options p s v = Maybe (Rules p s v)
-
 data ResolutionStep p s v = ResolutionStep { goal :: Query p s v
                                            , varsValues :: Substitution s v
-                                           , options :: Options p s v
+                                           , options :: Rules p s v
                                            , lastRID :: v
                                            } deriving (Show)
 
@@ -49,21 +47,21 @@ type Next a = a -> a
 
 type DataBase p s v db = db (Rules p s v)
 
-step :: (Eq p, Eq s, Eq v, KC.KeyedCollection db p) => Next v -> DataBase p s v db -> ResolutionState p s v -> ResolutionPath p s v -> Options p s v -> ResolutionState p s v
-step nextRID db retry p rs = maybe retry firstThatUnifies rs
+step :: (Eq p, Eq s, Eq v, KC.KeyedCollection db p) => Next v -> DataBase p s v db -> ResolutionState p s v -> ResolutionPath p s v -> Rules p s v -> ResolutionState p s v
+step nextRID db retry p rs = firstSolution rs
     where h = head p
           g = goal h
           s = varsValues h
           rid = lastRID h
           target = U.substituteTerms s $ head g
-          firstThatUnifies []     = retry
-          firstThatUnifies (r:rs) = case U.unify target $ rename rid $ T.ruleHead r of
-                                        Nothing -> firstThatUnifies rs
-                                        (Just s') -> do
-                                                       let body = map (rename rid) $ T.body r
-                                                       let uh = ResolutionStep g s (Just rs) rid
-                                                       let nh = ResolutionStep (body ++ (tail g)) (U.compose s' s) Nothing $ nextRID rid
-                                                       (S.put $ nh:uh:(tail p)) >> (down nextRID db)
+          firstSolution []     = retry
+          firstSolution (r:rs) = case U.unify target $ rename rid $ T.ruleHead r of
+                                     Nothing -> firstSolution rs
+                                     (Just s') -> do
+                                                    let body = map (rename rid) $ T.body r
+                                                    let uh = ResolutionStep g s rs rid
+                                                    let nh = ResolutionStep (body ++ (tail g)) (U.compose s' s) [] $ nextRID rid
+                                                    (S.put $ nh:uh:(tail p)) >> (down nextRID db)
 
 backtrack :: (Eq p, Eq s, Eq v, KC.KeyedCollection db p) => Next v -> DataBase p s v db -> ResolutionState p s v
 backtrack nextRID db = do
@@ -79,7 +77,8 @@ down nextRID db = do
                     let g = goal s
                     if null g
                       then return $ Just $ varsValues s
-                      else step nextRID db (backtrack nextRID db) p $ KC.find db $ OU.identifier $ T.predSymbol $ head g
+                      else let retry = backtrack nextRID db
+                           in maybe retry (step nextRID db retry p) $ KC.find db $ OU.identifier $ T.predSymbol $ head g
 
 rename :: v -> OU.Atom p s v -> OU.Atom p s (v, v)
 rename rid = fmap ((,) rid)
@@ -88,7 +87,7 @@ result :: (Eq v) => v -> ResolutionState p s v -> ResolutionPath p s v -> Resolu
 result rid st path = bimap (fmap (filter ((rid ==). fst . fst))) id $ S.runState st path
 
 initialResolutionPath :: Next v -> v -> OU.Query p s v -> ResolutionPath p s v
-initialResolutionPath nextRID rid q = [ResolutionStep (map (rename rid) q) U.empty Nothing $ nextRID rid]
+initialResolutionPath nextRID rid q = [ResolutionStep (map (rename rid) q) U.empty [] $ nextRID rid]
 
 resolve :: (Eq p, Eq s, Eq v, KC.KeyedCollection db p) => Next v -> v -> DataBase p s v db -> OU.Query p s v -> ResolutionResult p s v
 resolve nextRID rid db q = result rid (down nextRID db) $ initialResolutionPath nextRID rid q
