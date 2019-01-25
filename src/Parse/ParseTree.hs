6a3acdef85098410fe49e78058e072a1b393daad
NoHomey <ivo.stratev.tues@gmail.com>
2019-01-25 20:35:55 +0200

    refactoring Parse.ParseTree

--- src/Parse/ParseTree.hs
+++ src/Parse/ParseTree.hs
@@ -9,60 +9,63 @@ module Parse.ParseTree (
 import Parse.Grammar
 import Data.List
 import Data.Either
+import Data.Bifunctor
 
 data ParseTree n t = ParseTree {symbol :: Symbol n t, children :: [ParseTree n t]} deriving (Eq, Show)
 
-type ParseResult n t s = Either s (ParseTree n t)
-
 type State t s = ([t], s)
 
-type ParsingResult n t s = [(ParseResult n t s, State t s)]
+type ParseResult n t s = Either [s] [(ParseTree n t, State t s)]
 
 type UpdateState t s = s -> t -> s
 
 parseTreeLeaf :: Symbol n t -> ParseTree n t
 parseTreeLeaf s = ParseTree s []
 
-parseTerminal :: (Eq t) => UpdateState t s -> t -> State t s -> ParsingResult n t s
-parseTerminal u t s@(w, p) = [if null w || (head w) /= t
-                                then (Left p, s)
-                                else (Right $ parseTreeLeaf $ Terminal t, (tail w, u p t))]
+parseError :: s -> ParseResult n t s
+parseError s = Left [s]
 
-parseEnd :: ([t], s) -> ParsingResult n t s
-parseEnd s@(w, p) = [if null w then (Right $ parseTreeLeaf End, s) else (Left p, s)]
+accept :: ParseTree n t -> State t s -> ParseResult n t s
+accept pt s = Right [(pt, s)]
 
-parseNonTerminal :: (Eq t) => Grammar n t -> UpdateState t s -> n -> State t s -> ParsingResult n t s
+parseTerminal :: (Eq t) => UpdateState t s -> t -> State t s -> ParseResult n t s
+parseTerminal u t (w, p) = if null w || (head w) /= t
+                             then parseError p
+                             else accept (parseTreeLeaf $ Terminal t) (tail w, u p t)
+
+parseEnd :: State t s -> ParseResult n t s
+parseEnd s = if null $ fst s
+               then accept (parseTreeLeaf End) s
+               else parseError $ snd s
+
+parseNonTerminal :: (Eq t) => Grammar n t -> UpdateState t s -> n -> State t s -> ParseResult n t s
 parseNonTerminal g u n s = parse (NonTerminal n) s
     where parse (Skip n)         = parseNonTerminalSymbol Skip n
           parse (NonTerminal n)  = parseNonTerminalSymbol NonTerminal n
           parse (Terminal t)     = parseTerminal u t
           parse End              = parseEnd
-          parseNonTerminalSymbol symbol n s = case tryApply (body $ rules g n) s of
-                                                  (Right rs) -> [(Right $ ParseTree (symbol n) cs, st) | (cs, st) <- rs]
-                                                  (Left es) -> es 
-          apply state []     = Right [([], state)]
-          apply state (x:xs) = let (es, ts) = partition (isLeft . fst) $ parse x state
-                               in if null ts
-                                    then Left es
-                                    else let (es', cs) = partition (isLeft . fst) [(apply s xs, fromRight undefined p) | (p, s) <- ts]
-                                         in if null cs
-                                              then Left $ concatMap ((fromLeft undefined) . fst) es'
-                                              else Right $ if shouldSkip x
-                                                             then concatMap ((fromRight undefined) . fst) cs                  
-                                                             else [(t:ts, s) | (rts, t) <- cs, (ts, s) <- fromRight undefined rts]
-          tryApply bs state = let (es, cs) = partitionEithers $ map (apply state) bs
-                              in if null cs
-                                   then Left $ concat es
-                                   else Right $ concat cs
+          parseNonTerminalSymbol symbol n s = fmap (map $ first $ ParseTree $ symbol n) $ applyAll (body $ rules g n) s
+          apply st []     = Right [([], st)]
+          apply st (x:xs) = case parse x st of
+                                (Right ts) -> let (es, rs) = partitionEithers [fmap ((,) pt) $ apply s xs | (pt, s) <- ts]
+                                              in if null rs
+                                                   then Left $ concat es
+                                                   else Right $ if shouldSkip x
+                                                                  then concatMap snd rs
+                                                                  else [(t:ts, s) | (t, cts) <- rs, (ts, s) <- cts]
+                                (Left es) -> Left es
+          applyAll bs st = let (es, cs) = partitionEithers $ map (apply st) bs
+                           in if null cs
+                                then Left $ concat es
+                                else Right $ concat cs
           shouldSkip (Skip _) = True
           shouldSkip End      = True
           shouldSkip _        = False
 
 parse :: (Eq t) => Grammar n t -> UpdateState t s -> s -> n -> [t] -> Either [s] [ParseTree n t]
-parse g u p n w = let (es, ps) = partition (isLeft . fst) $ parseNonTerminal g u n (w, p)
-                      (ws, rrs) = partition (not . null . fst . snd) ps
-                      allEs = (map ((fromLeft undefined) . fst) es) ++ [snd $ snd wrong | wrong <- ws]
-                      rs = map ((fromRight undefined) . fst) rrs
-                  in if null rs
-                        then Left allEs
-                        else Right rs
+parse g u p n w = case parseNonTerminal g u n (w, p) of
+                      (Right ps) -> let (ws, rs) = partition (not . null . fst . snd) ps
+                                    in if null rs
+                                         then Left $ map (snd . snd) ws
+                                         else Right $ map fst rs
+                      (Left es) -> Left es
